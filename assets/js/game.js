29ede26496e04d1f32b25cebd18126255bf39c48
Josevillarr <jsuperman5@hotmail.com>
2021-12-11 23:36:27 -0800

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -59,6 +59,12 @@ var fight = function(enemyName) {
 
     }
 };
+  var startGame = function() {
+      playerHealth = 100;
+      playerAttack = 10;
+      playerMoney = 10;
+
+      
 
     for (var i = 0; i < enemyNames.length; i++) {
 
@@ -75,4 +81,33 @@ var fight = function(enemyName) {
             window.alert("You have lost your robot in battle! Game Over!");
             break;
         }
+        
+    }    
+    endGame(); 
+
+};
+var endGame = function() {
+
+
+    if (playerHealth > 0) {
+
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
     }
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+    
+    if (playAgainConfirm) {
+        
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!")
+    }
+    
+}
+
+startGame();
+
