fd3ebd1f76575fe823069a4595bf3317bbebdf7f
Josevillarr <jsuperman5@hotmail.com>
2021-12-12 12:28:54 -0800

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,6 +13,11 @@ console.log(enemyNames.length);
 console.log(enemyNames[0]);
 console.log(enemyNames[3]);
 
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+};
 
 
 var fight = function(enemyName) {
@@ -28,14 +33,15 @@ var fight = function(enemyName) {
             if (confirmSkip) {
 
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
 
                 console.log("playerMoney", playerMoney);
                 break;
             }
         }
 
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
 
@@ -47,7 +53,8 @@ var fight = function(enemyName) {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
 
-        playerHealth = playerHealth - enemyAttack;
+        var damage  = randomNumber(enemyAttack -3, enemyAttack);
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health reamining.");
 
         if (playerHealth <= 0) {
@@ -72,7 +79,7 @@ var fight = function(enemyName) {
             window.alert("Welcome to Robot Gladiators! Round " +  (i + 1));
 
             var pickedEnemyName = enemyNames[i];
-            enemyHealth = 50;        
+            enemyHealth = randomNumber(40, 60);
 
             fight(pickedEnemyName);
             
